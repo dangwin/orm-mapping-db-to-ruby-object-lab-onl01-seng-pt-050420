1b4254efaa9f2569a21657b7a5d6c06f140ae368
Dang Nguyen <danghwin@gmail.com>
2020-07-11 16:20:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,7 +87,7 @@ end
     end
   end
   
-    def self.first_X_students_in_grade_10
+    def self.first_X_students_in_grade_10(x)
         sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
